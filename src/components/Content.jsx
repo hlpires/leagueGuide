9bae38bea630f460ea886cdacbc8b4a6ea5faf27
hlpires <higorlpires@gmail.com>
2022-12-15 22:27:58 -0300

    initial software

---
+++ src/components/Content.jsx
@@ -0,0 +1,355 @@
+import React from 'react'
+import AKALI from './imgs/AKALI.png'
+import LEESIN from './imgs/ZED.png'
+import ZED from './imgs/LEESIN.png'
+import KLED from './imgs/KLED.png'
+import IGNITE from './imgs/IGNITE.png'
+import EXAUST from './imgs/EXAUST.png'
+
+const Content = () => {
+    {/*AKALI --> (Exaust e Ignite) Zed, Kled, (Rengar = ganhavel), Lee sin
+    NOCTURNE --> (Exaust e Ignite) Khazix, Akali, Warwick, Leblanc
+    REKSAI --> (Barreira e Ignite), Kled, Nocturne, Rumble, kha'zix
+    KHAZIX --> (Barreira e Ignite) Reksai, Rengar, Aatrox, Akali
+    RUMBLE --> (Exaust e Ignite) Aatrox(item = berrante), Darius, Rengar, Olaf
+    LEE'SIN --> (Exaust e Ignite) Darius, Renekton, Aatrox, Rek'sai, Nocturne, Warwick
+    RENGAR --> (Exaust e Ignite) Darius, Aatrox, Nocturne, Irelia
+    ZED --> (Exaust e Ignite) Kha zix, Rumble, Rek`sai, Lee sin, Udyr
+    BARDO --> (Exaust e Ignite) Darius, Rumble
+    LEBLANC --> (Barreira ou Exaust e Ignite) Akali, Rumble, Lee sin, Khazix
+    TRYNDAMERE --> BAN
+    RENEKTON --> (Exaust e Ignite) Quinn, Rumble, Kennen, Aatrox, Leblanc
+    KLED --> (Exaust e Ignite) Warwick, Zed, Leblanc
+    AATROX --> (Exaust e Ignite, Bolinha e ig pra noc)Nocturne, Akali, Irelia, Kled */}
+
+    const heros = [
+           {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+       {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+            foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+        {
+           foto: AKALI,
+            runaPrimaria:'',
+            runaSecundaria:'',
+            counters: {
+                0:'32312312',
+                1:'1323123',
+                2:'312312341'
+            },
+            good:{
+                zed:{
+                    imagem:ZED,
+                   nome:'Zed'
+                },
+                leesin:{
+                    imagem:LEESIN,
+                    nome:'Leesin'
+                },
+                kled:{
+                    imagem:KLED,
+                    nome:'Kled'
+                },
+            },
+            spell:{
+                0:IGNITE,
+                1:EXAUST
+            }
+        },
+    ]
+
+
+console.log(heros)  
+  return (
+    <div style = {{display:'flex',justifyContent:'center',marginTop:'100px'}}>
+        <div className = 'position'>
+           
+         {heros.map(({foto,counters,good,spell}) =>{
+             return(
+                 <div style={{width:'30%', border:'1px solid red', height:'60vh',float:'left',marginLeft:'2.6%',marginTop:'20px'}}>
+
+                 </div>
+             )
+         })}
+   
+        </div>
+    </div>
+  )
+}
+
+export default Content
