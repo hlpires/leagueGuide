ca7bc861ec3059f36d0197cad0cc0498c2902606
Higor Pires <higorlpires@gmail.com>
2022-12-21 14:36:01 -0300

    commit position runes addpting

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import AKALI from './imgs/AKALI.png'
 import LEESIN from './imgs/ZED.png'
 import ZED from './imgs/LEESIN.png'
@@ -10,6 +10,10 @@ import CELERIDADE from './imgs/celeridade.png'
 import CONQUEROR from './imgs/conqueror.png'
 import MORTE from './imgs/morte.png'
 import PRESENCA from './imgs/presenca.png'
+import CHAMUSCAR from './imgs/chamuscar.png'
+import TRANSCENDENCIA from './imgs/Transcendence.png'
+import FEITICARIA from './imgs/8200.webp'
+
 
 const Content = () => {
     {/*AKALI --> (Exaust e Ignite) Zed, Kled, (Rengar = ganhavel), Lee sin
@@ -337,56 +341,56 @@ const Content = () => {
             }
         },
     ]
-   const [estado,setEstado] = useState(true)
+    const [estado, setEstado] = useState(true)
 
     console.log(heros)
     return (
-        <div  className = 'imageBc' style={{ display: 'flex', justifyContent: 'center' }}>
+        <div className='imageBc' style={{ display: 'flex', justifyContent: 'center' }}>
             <div className='position'>
 
                 {heros.map(({ foto, counters, good, spell }) => {
                     console.log(foto)
                     return (
-                        <div  style={{ width: '28%', height: '60vh', float: 'left', marginLeft: '4%', marginTop: '50px', maxHeight: '600px',backgroundColor:'#323336',boxShadow:'rgb(0 0 0 / 19%) 0px 10px 20px, rgb(0 0 0 / 23%) 0px 6px 6px'}}>
-                                             
+                        <div style={{ width: '28%', height: '75vh', float: 'left', marginLeft: '4%', marginTop: '50px', maxHeight: '700px', backgroundColor: '#323336', boxShadow: 'rgb(0 0 0 / 19%) 0px 10px 20px, rgb(0 0 0 / 23%) 0px 6px 6px' }}>
+
                             <div style={{ display: 'flex', height: '25%' }}>
                                 <div style={{ width: '30%', height: '100%' }}>
                                     <img src={foto} style={{ width: '100%', height: '100%' }}></img>
                                 </div>
-                                <div style={{ marginLeft: '5%',  height: '100%', width: '60%',fontSize:'35px',fontWeight:'bolder' }}>
-                                    <div style = {{width:'100%',height:'50%',display:'flex'}}>
-                                    <div style = {{alignSelf:'center'}}>Akali</div>
+                                <div style={{ marginLeft: '5%', height: '100%', width: '60%', fontSize: '35px', fontWeight: 'bolder' }}>
+                                    <div style={{ width: '100%', height: '50%', display: 'flex' }}>
+                                        <div style={{ alignSelf: 'center' }}>Akali</div>
                                     </div>
-                                    <div style = {{width:'100%',height:'50%',display:'flex'}}>
-                                        <img src = {IGNITE} style = {{width:'25%',height:'100%'}}></img>
-                                        <img src = {EXAUST} style = {{marginLeft:'5%',width:'25%',height:'100%'}}></img>
-                                        <div style = {{fontSize:'20px', alignSelf:'center', marginLeft:'5%'}}>Tier: <span style = {{color:'red'}}>GOD</span></div>
+                                    <div style={{ width: '100%', height: '50%', display: 'flex' }}>
+                                        <img src={IGNITE} style={{ width: '25%', height: '100%' }}></img>
+                                        <img src={EXAUST} style={{ marginLeft: '5%', width: '25%', height: '100%' }}></img>
+                                        <div style={{ fontSize: '20px', alignSelf: 'center', marginLeft: '5%' }}>Tier: <span style={{ color: 'red' }}>GOD</span></div>
                                     </div>
                                 </div>
                             </div>
-                            <div style = {{border:'1px solid red',marginTop:'5%',width:'90%',marginLeft:'5%'}}>
-                                <button style = {{width:'20%',height:'100%', backgroundColor:'rgb(30, 30, 30)',marginLeft:'2%',padding:'2%',color:'white' }}>Runa P</button>
-                                <button style = {{width:'20%',height:'100%', backgroundColor:'rgb(30, 30, 30)',marginLeft:'5%',padding:'2%',color:'white'}}>Runa S</button>
-                                <button style = {{width:'20%',height:'100%', backgroundColor:'rgb(30, 30, 30)',marginLeft:'5%',padding:'2%',color:'white'}}>Counters</button>
-                                <button style = {{width:'20%',height:'100%', backgroundColor:'rgb(30, 30, 30)',marginLeft:'5%',padding:'2%',color:'white',cursor:'pointer'}}>Buffers</button>
+                            <div style={{ marginTop: '5%', width: '90%', marginLeft: '5%' }}>
+
+                                <button style={{ width: '20%', height: '100%', backgroundColor: 'rgb(30, 30, 30)', marginLeft: '14.5%', padding: '2%', color: 'white' }}>Runas</button>
+                                <button style={{ width: '20%', height: '100%', backgroundColor: 'rgb(30, 30, 30)', marginLeft: '5%', padding: '2%', color: 'white' }}>Counters</button>
+                                <button style={{ width: '20%', height: '100%', backgroundColor: 'rgb(30, 30, 30)', marginLeft: '5%', padding: '2%', color: 'white', cursor: 'pointer' }}>Buffers</button>
                             </div>
-                            {estado ? <div style={{border:'1px solid red',height:'58%', width:'90%',marginLeft:'5%',marginTop:'4%'}}>
-                            <div style={{width:'100%',height:'22%',border:'1px solid yellow',display:'flex',marginTop:'2%',justifyContent:'center'}}>
-                                    <img src = {CONQUEROR} style = {{width:'20%',border:'1px solid red',height:'100%'}}></img>
-                                    <img style = {{width:'25%',border:'1px solid red',height:'100%',marginLeft:'10%'}}></img>
+                            {estado ? <div className='runaBox' style={{ padding: '10px', height: '56%', width: '90%', marginLeft: '5%', marginTop: '4%' }}>
+                                <div style={{ width: '100%', height: '33%', display: 'flex', justifyContent: 'center' }}>
+                                    <img src={CONQUEROR} style={{ width: '26%', height: '100%' }}></img>
+                                    <img src={FEITICARIA} style={{ width: '25%', height: '100%', marginLeft: '10%' }}></img>
                                 </div>
-                                <div style={{width:'100%',height:'22%',border:'1px solid yellow',display:'flex',marginTop:'2%',justifyContent:'center'}}>
-                                <img  src = {PRESENCA} style = {{width:'16%',border:'1px solid red',height:'80%',marginTop:'2%'}}></img>
-                                    <img style = {{width:'25%',border:'1px solid red',height:'100%',marginLeft:'10%'}}></img>
+                                <div style={{ width: '100%', height: '18%', display: 'flex', justifyContent: 'center', marginTop: '2%' }}>
+                                    <img src={PRESENCA} style={{ width: '12%', height: '90%', marginTop: '2%' }}></img>
+                                    <img src={TRANSCENDENCIA} style={{ width: '12%', height: '90%', marginTop: '2%', marginLeft: '23%' }}></img>
                                 </div>
-                                <div  style={{width:'100%',height:'22%',border:'1px solid yellow',display:'flex',marginTop:'2%',justifyContent:'center'}}>
-                                <img src = {CELERIDADE} style = {{width:'16%',border:'1px solid red',height:'80%',marginTop:'2%'}}></img>
-                                    <img style = {{width:'25%',border:'1px solid red',height:'100%',marginLeft:'10%'}}></img>
+                                <div style={{ width: '100%', height: '18%', display: 'flex', justifyContent: 'center', marginTop: '2%' }}>
+                                    <img src={CELERIDADE} style={{ width: '12%', height: '90%', marginTop: '2%' }}></img>
+                                    <img src={CHAMUSCAR} style={{ width: '12%', height: '90%', marginTop: '2%', marginLeft: '23%' }}></img>
                                 </div>
-                                <div  style={{width:'100%',height:'22%',border:'1px solid yellow',display:'flex',marginTop:'2%',justifyContent:'center',marginRight:'5%'}}>
-                                    <img src = {MORTE} style = {{width:'16%',border:'1px solid red',height:'80%',marginRight:'35%'}}></img>
+                                <div style={{ width: '100%', height: '18%', display: 'flex', justifyContent: 'center', marginRight: '5%', marginTop: '2%' }}>
+                                    <img src={MORTE} style={{ width: '12%', height: '90%', marginTop: '2%', marginRight: '35%' }}></img>
                                 </div>
-                                
+
                             </div> : <div></div>}
 
                         </div>
