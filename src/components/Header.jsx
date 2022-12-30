cd8453680e840411673e7fd9d479173a4b475c0f
hlpires <higorlpires@gmail.com>
2022-12-30 16:58:45 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
     <div style={{ width: '100vw', backgroundColor: 'black', opacity: '60%', height: '100px', position: 'fixed', zIndex: '99' }}>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div style={{ width: '92vw', maxWidth: '2400px' }}>
-          <a href='https://www.twitch.tv/inefavel1'><img alt='' src={SAMUCA} style={{ width: '120px', height: '100px', float: 'left' }}>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.twitch.tv/inefavel1'><img alt='' src={SAMUCA} style={{ width: '120px', height: '100px', float: 'left' }}>
 
           </img></a>
-          <p style={{ float: 'left', color: 'white', marginLeft: '2%', marginTop: '2.5%', fontSize: '30px' }}>TWTV/INEFAVEL1</p>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.twitch.tv/inefavel1'><div> <p style={{ float: 'left', color: 'white', marginLeft: '2%', marginTop: '2.5%', fontSize: '30px',cursor:'pointer' }}>TWTV/INEFAVEL1</p></div></a>
         </div>
       </div>
     </div >
