9e4801b4b4304333f7623d0cac57453a30992211
hlpires <higorlpires@gmail.com>
2022-12-30 18:39:59 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className = 'footer' ><p style = {{alignSelf:'center'}}>Developed by HLE DA CHINA © 2022. Todos os direitos reservados.</p></div>
+    <div className = 'footer' ><p style = {{alignSelf:'center'}}>Developed by HLE DA CHINA © 2022. </p></div>
   )
 }
 
