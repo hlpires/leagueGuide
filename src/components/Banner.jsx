cd938f54fe5ff24b4d92fa68d9f5c4685a17484a
hlpires <higorlpires@gmail.com>
2022-12-30 18:44:12 -0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <div className='banner' style={{ height: '80vh', maxHeight: '1100px',display:'flex',justifyContent:'center' }}>
             <div style ={{alignSelf:'center',maxWidth:'1000px'}}>
-            <TwitchEmbed channel="inefavel1" width = {1000} autoplay muted withChat = {false} darkMode={true} hideControls onVideoReady={handleReady} />
+            <TwitchEmbed channel="inefavel1" width = {1000} autoplay = {false} muted withChat = {false} darkMode={true} hideControls onVideoReady={handleReady} />
             </div>
             </div>
         </div >
