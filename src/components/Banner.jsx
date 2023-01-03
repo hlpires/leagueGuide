8cced493fca006102110ebf5d66bee4bcc936acc
hlpires <higorlpires@gmail.com>
2023-01-03 01:10:20 -0300

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -18,8 +18,8 @@ const Banner = () => {
             </div>
             <div className = 'pix' style = {{width:'400px',height:'484px',alignSelf:'center', marginLeft:'50px',justifyContent:'center'}}>
             <div style = {{width:'90%',height:'35%',marginTop:'5%',marginLeft:'5%'}}>
-                <h1 style = {{color:'white',textAlign:'center'}}>Ajude nos a fazer mais</h1>
-                <p  style = {{color:'white',textAlign:'center',marginTop:'10px',fontSize:'20px'}}>Criamos esse site com o intuito de ajudar toda a comunidade x1, ao doar Você também nos ajuda a criar novos recursos para você usar, e ficar fera no x1 !</p>
+                <h1 style = {{color:'white',textAlign:'center'}}>Ajude-nos a fazer mais</h1>
+                <p  style = {{color:'white',textAlign:'center',marginTop:'10px',fontSize:'20px'}}>Criamos esse site com o intuito de ajudar toda a comunidade de x1. Ao doar você também nos ajuda a criar novos recursos para você usar e ficar fera no x1 !</p>
             </div>
             <div style = {{width:'250px',height:'250px',marginTop:'5%',marginLeft:'18.5%'}}>
             <img src={pix} style = {{width:'100%',height:'100%'}}></img>
